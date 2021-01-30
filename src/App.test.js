ca9a36cd6df08a3477e478a3c6cf5587b4dd54a7
Anand Shastri <anandshastri1990@gmail.com>
2021-01-30 17:45:54 -0500

    should accept player 2 name

--- src/App.test.js
+++ src/App.test.js
@@ -8,10 +8,20 @@ it('should contain application header Tic Tac Toe', () => {
     expect(component.find('#header').text()).toEqual('Tic Tac Toe');
 });
 
-it('should accept player 1 name', () => {
-    const component = shallow(<App/>);
+describe('Player name', () => {
+    it('should accept player 1 name', () => {
+        const component = shallow(<App/>);
 
-    component.find('#player1').simulate('change', {target: {value: 'Custom name 1'}})
+        component.find('#player1').simulate('change', {target: {value: 'Custom name 1'}})
 
-    expect(component.find('#player1').prop('value')).toContain('Custom name 1');
-});
+        expect(component.find('#player1').prop('value')).toContain('Custom name 1');
+    });
+
+    it('should accept player 2 name', () => {
+        const component = shallow(<App/>);
+
+        component.find('#player2').simulate('change', {target: {value: 'Custom name 2'}})
+
+        expect(component.find('#player2').prop('value')).toContain('Custom name 2');
+    });
+})
