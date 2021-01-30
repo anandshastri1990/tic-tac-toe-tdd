99ad39472033a7bad843dcfb63b685bf153f0ba0
Anand Shastri <anandshastri1990@gmail.com>
2021-01-30 17:43:52 -0500

    accept player 1 name

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 import React from "react";
 
 export class App extends React.Component {
+    state = {player1Name: ""}
+
     render() {
-        return <div id={"header"}>Tic Tac Toe</div>
+        return <div>
+            <div id={"header"}>Tic Tac Toe</div>
+
+            <input id={"player1"}
+                   value={this.state.player1Name}
+                   onChange={($event) => {
+                       this.setState({player1Name: $event.target.value})
+                   }}/>
+        </div>
     }
 }
 
