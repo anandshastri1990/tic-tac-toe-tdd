150ff65db4a1a2fc6bf4b35db5a874ee0cc37174
Anand Shastri <anandshastri1990@gmail.com>
2021-01-30 17:41:34 -0500

    should accept player 1 name

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,16 @@ import React from "react";
 import {shallow} from "enzyme";
 import App from "./App";
 
-test('should contain application header Tic Tac Toe', () => {
+it('should contain application header Tic Tac Toe', () => {
     const component = shallow(<App/>);
 
     expect(component.find('#header').text()).toEqual('Tic Tac Toe');
+});
+
+it('should accept player 1 name', () => {
+    const component = shallow(<App/>);
+
+    component.find('#player1').simulate('change', {target: {value: 'Custom name 1'}})
+
+    expect(component.find('#player1').prop('value')).toContain('Custom name 1');
 });
