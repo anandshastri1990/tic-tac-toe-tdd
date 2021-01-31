432ee6f0d25778aee752f167072df45372936977
Anand Shastri <anandshastri1990@gmail.com>
2021-01-30 19:45:08 -0500

    divide game board into 3x3 boxes

--- src/App.js
+++ src/App.js
@@ -30,15 +30,21 @@ export class App extends React.Component {
             </div>
 
             <div id="game-board">
-                <div id={"box-1"} onClick={() => this.onBoxClicked(0)}>{this.state.gameBoard[0]}</div>
-                <div id={"box-2"} onClick={() => this.onBoxClicked(1)}>{this.state.gameBoard[1]}</div>
-                <div id={"box-3"} onClick={() => this.onBoxClicked(2)}>{this.state.gameBoard[2]}</div>
-                <div id={"box-4"} onClick={() => this.onBoxClicked(3)}>{this.state.gameBoard[3]}</div>
-                <div id={"box-5"} onClick={() => this.onBoxClicked(4)}>{this.state.gameBoard[4]}</div>
-                <div id={"box-6"} onClick={() => this.onBoxClicked(5)}>{this.state.gameBoard[5]}</div>
-                <div id={"box-7"} onClick={() => this.onBoxClicked(6)}>{this.state.gameBoard[6]}</div>
-                <div id={"box-8"} onClick={() => this.onBoxClicked(7)}>{this.state.gameBoard[7]}</div>
-                <div id={"box-9"} onClick={() => this.onBoxClicked(8)}>{this.state.gameBoard[8]}</div>
+                <div id={"row-1"}>
+                    <div id={"box-1"} onClick={() => this.onBoxClicked(0)}>{this.state.gameBoard[0]}</div>
+                    <div id={"box-2"} onClick={() => this.onBoxClicked(1)}>{this.state.gameBoard[1]}</div>
+                    <div id={"box-3"} onClick={() => this.onBoxClicked(2)}>{this.state.gameBoard[2]}</div>
+                </div>
+                <div id={"row-2"}>
+                    <div id={"box-4"} onClick={() => this.onBoxClicked(3)}>{this.state.gameBoard[3]}</div>
+                    <div id={"box-5"} onClick={() => this.onBoxClicked(4)}>{this.state.gameBoard[4]}</div>
+                    <div id={"box-6"} onClick={() => this.onBoxClicked(5)}>{this.state.gameBoard[5]}</div>
+                </div>
+                <div id={"row-3"}>
+                    <div id={"box-7"} onClick={() => this.onBoxClicked(6)}>{this.state.gameBoard[6]}</div>
+                    <div id={"box-8"} onClick={() => this.onBoxClicked(7)}>{this.state.gameBoard[7]}</div>
+                    <div id={"box-9"} onClick={() => this.onBoxClicked(8)}>{this.state.gameBoard[8]}</div>
+                </div>
             </div>
 
         </div>
