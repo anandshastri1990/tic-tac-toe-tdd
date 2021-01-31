48a8c565d7fc029fe21a48cbeec6bd24c30a0bf9
Anand Shastri <anandshastri1990@gmail.com>
2021-01-30 20:50:47 -0500

    winning combo 3 - 3nd row

--- src/GameManager.test.ts
+++ src/GameManager.test.ts
@@ -1,9 +1,13 @@
 import {isWinningCombo} from "./GameManager";
 
-it('winning combo 1 - 1st row ', function () {
+it('winning combo 1 - 1st row', function () {
     expect(isWinningCombo(['X', 'X', 'X', 'O', 'O'])).toEqual(true);
 });
 
-it('winning combo 2 - 2nd row ', function () {
+it('winning combo 2 - 2nd row', function () {
     expect(isWinningCombo([null, null, 'X', 'O', 'O', 'O', 'X', 'X', null])).toEqual(true);
+});
+
+it('winning combo 3 - 3nd row', function () {
+    expect(isWinningCombo([null, null, null, 'O', 'O', null, 'X', 'X', 'X'])).toEqual(true);
 });
