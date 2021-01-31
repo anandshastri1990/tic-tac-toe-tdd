cd94c1694066ac94d81b7236922c6f853ad1bdae
Anand Shastri <anandshastri1990@gmail.com>
2021-01-30 20:56:14 -0500

    add winning combo 5 - 2nd column

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -4,5 +4,6 @@ export function isWinningCombo(allBoxes): boolean {
         || (allBoxes[3] !== null && allBoxes[3] === allBoxes[4] && allBoxes[3] === allBoxes[5])
         || (allBoxes[6] !== null && allBoxes[6] === allBoxes[7] && allBoxes[6] === allBoxes[8])
         || (allBoxes[0] !== null && allBoxes[0] === allBoxes[3] && allBoxes[0] === allBoxes[6])
+        || (allBoxes[1] !== null && allBoxes[1] === allBoxes[4] && allBoxes[1] === allBoxes[7])
     );
 }
